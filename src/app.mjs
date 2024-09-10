f8611ac18ee296d5404afb71db8abc6ad3f5f306
ayat_elsherif <ayat_elsherif@yahoo.com>
2024-09-10 21:16:07 +0300

    cleaning app of serveless

--- src/app.mjs
+++ src/app.mjs
@@ -2,13 +2,12 @@ import express from 'express';
 
 const app = express();
 
-export const handler = serverless(app);
 app.get('/', (req, res) => {
   res.json({ hello: 'hello world' });
 });
 
 app.get('/help', (req, res) => {
-  res.send('<h2>please hellllllp </h2>');
+  res.json({ help: 'trying to deploy to a server' });
 });
 
 app.listen(4000, () => {
